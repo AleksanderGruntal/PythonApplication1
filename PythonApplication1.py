2885940c52b1aea5d1e9666b894415518fbc24f7
opilane <opilane@HP-CZC6297BB4>
2022-12-13 10:00:19 +0200

    Add project files.

---
+++ PythonApplication1.py
@@ -0,0 +1,47 @@
+﻿#12 (4)
+from math import*
+from random import*
+print("распорядок дня")
+
+try: kell=int(input("mis kell  "))
+except:
+    print("!!!")
+if kell==(0):
+    print(f"kodus")
+elif kell==(1):
+    print("просыпаюсь")
+elif kell==(2):
+    print("чищю зубы")
+elif kell==(3):
+    print("дклвю заряду")
+elif kell==(4):
+    print("иду гулять")
+elif kell==(5):
+    print("пришол домой")
+elif kell==(6):
+    print("чищю зубы")
+elif kell==(7):
+    print("иду спать")
+elif kell==(8):
+    print("сплю")
+elif kell==(9):
+    print("сплю")
+elif kell==(10):
+    print("сплю")
+elif kell==(11):
+    print("сплю")
+elif kell==(12):
+    print("сплю")
+elif kell==(13):
+    print("сплю")
+elif kell==(14):
+    print("сплю")
+elif kell==(15):
+    print("сплю")
+elif kell==(16):
+    print("сплю")
+ 
+    
+
+
+
